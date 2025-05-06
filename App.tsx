65e5e63a7c57f3c29238806acd6eea7233848929
bflamson <bflamson1@gmail.com>
2025-05-05 19:16:01 -0700

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import Button from './button';
+import Card from './card';
+
+const App = () => (
+  <div>
+    <h1>Welcome to the Trading Psychology Bot</h1>
+    <Card>
+      <Button>Start</Button>
+    </Card>
+  </div>
+);
+
+export default App;
+    
